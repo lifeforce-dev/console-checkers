886dd6d4389032054f6c0edee2019a1a78394da8
Joshua Sanders <joshua.sandersr@gmail.com>
2024-07-31 22:40:58 -0700

    fix build

---
+++ premake/generated/package_info.lua
@@ -0,0 +1,13 @@
+return {
+    packages = {
+        spdlog = {
+            version = "ac55e604"
+        },
+        glm = {
+            version = "0.9.9.8"
+        },
+        observable = {
+            version = "v1.0.0"
+        }
+    }
+}
